5d2938f6c632ac1b57bb994fa8d0be892dca5486
Priya Balasubramanian <priyavb1207@gmail.com>
2024-09-17 13:59:13 -0700

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "88959935e5b3e71115c002a0e0c684d0"
+weather_api_key = "9f5f93531317726a0c261e966f322ee3"
 
 # Geoapify API Key
 geoapify_key = "YOUR KEY HERE"
